be89864d83ea5be3e48225db0720e5d1ffc08a26
Chris McKirgan <chris@mckirgan.com>
2015-07-03 16:31:07 +0100

    Fixed; tonnes of gross and unforgivable spelling mistakes.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -11,25 +11,25 @@
 		Baconize.it comes from a proud tradition of URL baconizing. Founded in 2015 B.C. in the ancient swine planes of Somerset, England by Baconize'it McKirgan, Baconize.it has been baconizing URLs for, like, literally forever. 
 	</p>
 	<p>
-		The Uniform Resource Locator, or <i>URL</i> wasn't created until shortly before 1994 by web pioneer Tim Berners-Lee, this however never stopped Baconize'it McKirgan from Baconizing URL for millenia. Prior to the invention of digital URLs Baconize'it McKirgan used delicatly hand crafted anologue URLs, painstakenly forged after extrating raw internet ore from the quaries of Weberhampton.
+		The Uniform Resource Locator, or <i>URL</i> wasn't created until shortly before 1994 by web pioneer Tim Berners-Lee, this however never stopped Baconize'it McKirgan from Baconizing URL for millennia. Prior to the invention of digital URLs Baconize'it McKirgan used delicately hand crafted anologue URLs, painstakingly forged after extracting raw internet ore from the quarries of Weberhampton.
 	</p>
 	<p>
-		The proud tradition of baconizing stems from the original practice used by Baconize'it McKirgan - heating the internet ore, slicing back cuts of swine meat and placing them ontop of the blazing ore. In what scientist Stephen Hawkins has recently described as a <i>quantum miracle</i>, both Baconized URLs and what we would refer to as modern bacon were formed.
+		The proud tradition of baconizing stems from the original practice used by Baconize'it McKirgan - heating the internet ore, slicing back cuts of swine meat and placing them on top of the blazing ore. In what scientist Stephen Hawking has recently described as a <i>quantum miracle</i>, both Baconized URLs and what we would refer to as modern bacon were formed.
 	</p>
 	<p>
 		Baconized URLs have somewhat fallen out of popularity with the rise of the digital age, whereas swine bacon has only grown in popularity. Thought largely to be due to the non-judgmental, all forgiving, transcendent love that bacon bestows upon its consumers. 
 	</p>
 	<p>
-		Baconized URLs were largely useless before the creation of the web. Primarily due to lack of DNS resolution 4 thausand years ago, amoung other minor technical factors. It wasn't until the 16th century that bacon as we know it today became colloquially known as 'bacon', known predominantly as internet slag to the nearby hillfolk of France. The contemporary bacon found common throughout modern England and, indeed, the rest of the world, still bears some resemblance to the name of the original architect, Baconize'it McKirgan.
+		Baconized URLs were largely useless before the creation of the web. Primarily due to lack of DNS resolution 4 thousand years ago, among other minor technical factors. It wasn't until the 16th century that bacon as we know it today became colloquially known as 'bacon', known predominantly as internet slag to the nearby hillfolk of France. The contemporary bacon found common throughout modern England and, indeed, the rest of the world, still bears some resemblance to the name of the original architect, Baconize'it McKirgan.
 	</p>
 	<p>
-		We are delighted to enter a new era of baconizing with the rise of the web, and we emplore you to join us in Baconizing digital URLs using the latest in Baconizing technology: <a href="{{$site_url}}">baconize.it</a>.
+		We are delighted to enter a new era of baconizing with the rise of the web, and we implore you to join us in Baconizing digital URLs using the latest in Baconizing technology: <a href="{{$site_url}}">baconize.it</a>.
 	</p>
 	<p>
 		May your URLs be ever Baconized,
 	</p>
 	<p>
-		- Christopher McKirgan, direct decendent of Baconize'it McKirgan, creator of the first Baconized URL.
+		- Christopher McKirgan, direct descendant of Baconize'it McKirgan, creator of the first Baconized URL.
 	</p>
 </article>
 @stop
